14274889085a9b3f63755c65e97859d7e0e59a84
Alexander Rook <arook8@gmail.com>
2017-10-28 14:45:53 -0400

    testing tcp streaming

--- Protocols/src/Client.java
+++ Protocols/src/Client.java
@@ -1,11 +1,12 @@
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 
 public class Client {
 
     public static final double g = Math.pow(2, 30);
     public static final double m = Math.pow(2, 20);
     
-    public static Socket getSocket(String host, int port) throws Exception
+    public static Socket getSocket(String host, int port)
     {
         try {
             Socket sock = new Socket(host, port);
@@ -18,18 +19,47 @@ public class Client {
         return null;
     }
     
+    public static void TCP_streaming(String host, int port, int size) throws Exception
+    {
+    	double count = g;
+    	byte[] bytes = new byte[size];
+    	Socket sock = getSocket(host, port);
+    	DataOutputStream toServer = new DataOutputStream(sock.getOutputStream());
+    	BufferedReader fromServer = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+    	
+    	toServer.writeBytes("0 " + g); //telling server: no ack, 1g total size
+    	long endTime = 0L;
+    	long startTime = System.currentTimeMillis();
+    	
+    	while(count > 0) {
+    		//if(bytes_sent != msg_size) {error}
+    		toServer.write(bytes);
+    		count -= bytes.length;
+    	}
+    	endTime = System.currentTimeMillis();
+    	long elapsedTime = endTime - startTime;
+    	long throughput = elapsedTime/(long)m;
+    	
+    	System.out.println(throughput);
+    	
+    }
+    
     public static void main(String[] args)
     {
         String host = args[0];
         int port = Integer.parseInt(args[1]);
-        String transport = args[2];
-        int msgSize = Integer.parseInt(args[3]);
-        String ack = args[4];
+        String transport = args[2]; //TCP or UDP
+        int msgSize = Integer.parseInt(args[3]); //1 byte to 65,536 bytes
+        String ack = args[4]; //pure streaming or stop-and-wait
 
         String response = "";
         if(transport.equals("TCP")) {
             if(ack.equals("streaming")) {
-                
+            	try {
+            		TCP_streaming(host, port, msgSize);
+            	} catch(Exception e) {
+            		System.err.println(e);
+            	}
             } else if(ack.equals("stop-and-wait")) {
                 
             } else {
--- Protocols/src/Server.java
+++ Protocols/src/Server.java
@@ -1,10 +1,54 @@
+import java.io.*;
+import java.net.*;
 
-public class Server {
-
-    public static void main(String[] args) {
+public class Server implements Runnable {
+	Socket conn;
+	
+	Server(Socket sock) {
+		this.conn = sock;
+	}
+	
+    public static void main(String[] args) throws Exception {
+        int port = Integer.parseInt(args[0]);
+        String transport = args[1];
+        
+        if(transport.equals("TCP")) {
+	        ServerSocket svc = new ServerSocket(port, 5);
+	        
+	        for(;;) {
+	        	Socket conn = svc.accept();
+	        	new Thread(new Server(conn)).start();
+	        }
+        } else if(transport.equals("UDP")) {
+        	
+        } else {
+        	System.err.println("Invalid Transport Protocol (TCP or UDP)");
+        }
+    }
     
-        
-        
-        
+    public void run() {
+    	try {
+    		int count = 0;
+    		double expectedSize = 0.0;
+    		BufferedReader fromClient = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+    		DataOutputStream toClient = new DataOutputStream(conn.getOutputStream());
+    		String line;
+    		line = fromClient.readLine();
+    		String[] input = line.split(" ");
+    		int ack = Integer.parseInt(input[0]);
+    		expectedSize = Double.parseDouble(input[1]);
+    		
+    		
+    		while((line = fromClient.readLine()) != null) {
+    			count += line.length();
+    			if(count >= expectedSize) {
+    				break;
+    			}
+    		}
+    		
+    		conn.close();
+    	} catch (IOException e) {
+    		System.err.println(e);
+    	}
     }
 }
