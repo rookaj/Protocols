bde083f753dfcad15ee845305f94812dbefb976a
rookaj <arook8@gmail.com>
2017-10-29 16:21:28 -0400

    update

--- Protocols/src/Client.java
+++ Protocols/src/Client.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+import java.nio.ByteBuffer;
 
 public class Client {
 
@@ -19,15 +20,22 @@ public class Client {
         return null;
     }
     
-    public static void TCP_streaming(String host, int port, int size) throws Exception
+    public static void TCPTimer(String host, int port, int size, int ack) throws Exception
     {
-    	int count = g;
+        int count = g/4096;
+    	int total = 0;
     	byte[] bytes = new byte[size];
+    	int msgCount = 0;
+    	
     	Socket sock = getSocket(host, port);
     	DataOutputStream toServer = new DataOutputStream(sock.getOutputStream());
-    	BufferedReader fromServer = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-    	System.out.println(g);
-    	toServer.writeByte(0);//telling server no ack
+    	DataInputStream fromServer = new DataInputStream(sock.getInputStream());
+    	
+    	if(ack == 0) {
+    	    toServer.writeByte(0); //telling server pure streaming
+    	} else if(ack == 1) {
+    	    toServer.writeByte(1); //telling server stop-and-wait
+    	}
     	
     	toServer.writeInt(count); //telling server: 1g total size
     	long endTime = 0L;
@@ -37,48 +45,116 @@ public class Client {
     		//if(bytes_sent != msg_size) {error}
     		toServer.write(bytes);
     		count -= bytes.length;
+    		total += bytes.length;
+    		msgCount++;
+    		if(ack == 1) {
+    		    fromServer.readByte();
+    		}
     	}
+    	
     	endTime = System.currentTimeMillis();
     	long elapsedTime = endTime - startTime;
-    	double throughput = ((double)g/(double)elapsedTime)*(1000/(double)m);
+    	double throughput = ((double)total/(double)elapsedTime)*(1000/(double)m);
     	
-    	System.out.println(throughput);
+        System.out.println("Messages Sent: " + msgCount);
+        System.out.println("Bytes Sent: " + total);
+        System.out.println("Total Transmit Time: " + elapsedTime + " milliseconds");
+    	System.out.println("Throughput: " + throughput);
     	sock.close();
+    }
+    
+    public static void UDPTimer(String host, int port, int size, int ack) throws Exception
+    {
+        int count = g/4096;
+        int total = 0;
+        int msgCount = 0;
+        
+        byte[] ackType = new byte[1];
+        byte[] totalSize = ByteBuffer.allocate(4).putInt(count).array();
+        if(ack == 0) {
+           ackType[0] = 0; //telling server pure streaming
+        } else if(ack == 1) {
+            ackType[0] = 1; //telling server stop-and-wait
+        }
+        byte[] bytes = new byte[size];
+        byte[] receiveAck = new byte[1];
+        DatagramSocket sock = new DatagramSocket();
+        InetAddress IPAddress = InetAddress.getByName(host);
+        
+        DatagramPacket sendAck = new DatagramPacket(ackType, ackType.length, IPAddress, port);
+        DatagramPacket sendSize = new DatagramPacket(totalSize, totalSize.length, IPAddress, port);
+        DatagramPacket receivePacket = new DatagramPacket(receiveAck, receiveAck.length);
+        
+        sock.send(sendAck);
+        sock.receive(receivePacket);
+        sock.send(sendSize);
+        sock.receive(receivePacket);
+        DatagramPacket sendPacket = new DatagramPacket(bytes, bytes.length, IPAddress, port);
+        
+        
+        long endTime = 0L;
+        long startTime = System.currentTimeMillis();
+        
+        while(count > 0) {
+            sock.send(sendPacket);
+            count -= bytes.length;
+            total += bytes.length;
+            msgCount++;
+            if(ack == 1) {
+                sock.receive(receivePacket);
+            }
+            
+        }
+        
+        endTime = System.currentTimeMillis();
+        long elapsedTime = endTime - startTime;
+        double throughput = ((double)total/(double)elapsedTime)*(1000/(double)m);
+        
+        System.out.println("Messages Sent: " + msgCount);
+        System.out.println("Bytes Sent: " + total);
+        System.out.println("Total Transmit Time: " + elapsedTime + " milliseconds");
+        System.out.println("Throughput: " + throughput);
+        sock.close();
     }
     
     public static void main(String[] args)
     {
+        if(args.length != 5) {
+            System.err.println("Invalid Number of Arguments.");
+            System.exit(1);
+        }
+        
         String host = args[0];
         int port = Integer.parseInt(args[1]);
         String transport = args[2]; //TCP or UDP
         String ack = args[3]; //pure streaming or stop-and-wait
         int msgSize = Integer.parseInt(args[4]); //1 byte to 65,536 bytes
         
-        String response = "";
-        if(transport.equals("TCP")) {
-            if(ack.equals("streaming")) {
-            	try {
-            		TCP_streaming(host, port, msgSize);
-            	} catch(Exception e) {
-            		System.err.println(e);
-            	}
-            } else if(ack.equals("stop-and-wait")) {
-                
+        try {
+            if(transport.equals("TCP")) {
+                if(ack.equals("streaming")) {
+                		TCPTimer(host, port, msgSize, 0);
+                } else if(ack.equals("stop-and-wait")) {
+                        TCPTimer(host, port, msgSize, 1);
+                } else {
+                    System.err.println("Invalid Acknowledgement Protocol (streaming or stop-and-wait)");
+                    System.exit(1);
+                }
+            } else if(transport.equals("UDP")) {
+                if(ack.equals("streaming")) {
+                    UDPTimer(host, port, msgSize, 0);
+                } else if(ack.equals("stop-and-wait")) {
+                    UDPTimer(host, port, msgSize, 1);
+                } else {
+                    System.err.println("Invalid Acknowledgement Protocol (streaming or stop-and-wait)");
+                    System.exit(1);
+                }
             } else {
-                System.err.println("Invalid Acknowledgement Protocol (streaming or stop-and-wait)");
+                System.err.println("Invalid Transport Protocol (TCP or UDP)");
                 System.exit(1);
             }
-        } else if(transport.equals("UDP")) {
-            if(ack.equals("streaming")) {
-                
-            } else if(ack.equals("stop-and-wait")) {
-                
-            } else {
-                System.err.println("Invalid Acknowledgement Protocol (streaming or stop-and-wait)");
-                System.exit(1);
-            }
-        } else {
-            System.err.println("Invalid Transport Protocol (TCP or UDP)");
+        } catch(Exception e) {
+            System.err.println(e);
             System.exit(1);
         }
     }
--- Protocols/src/Server.java
+++ Protocols/src/Server.java
@@ -11,52 +11,148 @@ public class Server implements Runnable {
 	}
 	
     public static void main(String[] args) throws Exception {
+        if(args.length != 2) {
+            System.err.println("Invalid Number of Arguments.");
+            System.exit(1);
+        }
+        
         int port = Integer.parseInt(args[0]);
         String transport = args[1];
         
         if(transport.equals("TCP")) {
-	        ServerSocket svc = new ServerSocket(port, 5);
-	        
-	        for(;;) {
-	        	Socket conn = svc.accept();
-	        	new Thread(new Server(conn)).start();
+            ServerSocket servSock = new ServerSocket(port, 5);
+            try {
+    	        
+    	        for(;;) {
+    	        	Socket conn = servSock.accept();
+    	        	new Thread(new Server(conn)).start();
+    	        }
+	        } catch(Exception e) {
+	            servSock.close();
+	        } finally {
+	            servSock.close();
 	        }
         } else if(transport.equals("UDP")) {
-        	
+            try {
+                UDPRun(port);
+            } catch(Exception e) {
+                System.err.println(e);
+            }
         } else {
         	System.err.println("Invalid Transport Protocol (TCP or UDP)");
+        	System.exit(1);
         }
     }
     
-    public void run() {
+    public static void UDPRun(int port) throws Exception
+    {
+        int msgCount = 0;
+        String ackProtocol = "Pure Streaming";
+        DatagramSocket servSock = new DatagramSocket(port);
+        
+        byte[] clientAck = new byte[1];
+        byte[] clientSize = new byte[5];
+        
+        DatagramPacket recvAck = new DatagramPacket(clientAck, clientAck.length);
+        DatagramPacket recvSize = new DatagramPacket(clientSize, clientSize.length);
+       
+        
+        servSock.receive(recvAck);
+       
+        InetAddress IPAddress = recvAck.getAddress();
+        int clientPort = recvAck.getPort();
+        DatagramPacket sendAck = new DatagramPacket(clientSize, clientSize.length, IPAddress, clientPort);
+        
+        servSock.send(sendAck);
+        servSock.receive(recvSize);
+        servSock.send(sendAck);
+        
+        ByteBuffer byteBuffer = ByteBuffer.wrap(recvSize.getData(), 0, 4);
+        int totalSize = byteBuffer.getInt();
+        byte ack = recvAck.getData()[0];
+        
+        byte[] msg = new byte[totalSize];
+        String s = "";
+        
+        if(ack == 1) {
+            byte[] ackArr = {ack};
+            sendAck = new DatagramPacket(ackArr, ackArr.length, IPAddress, clientPort);
+            ackProtocol = "Stop-and-Wait";
+        }
+        servSock.setSoTimeout(1000);
+
+        try{
+            while(s.length() < totalSize) {
+                  DatagramPacket receivePacket = new DatagramPacket(msg, msg.length);
+                  servSock.receive(receivePacket);
+                  s += new String(receivePacket.getData(), 0, receivePacket.getLength());
+                  msgCount++;
+                  
+                  if(ack == 1) {
+                      servSock.send(sendAck);
+                  }
+    
+            }
+        } catch(SocketTimeoutException e) {
+            System.out.println("Error: Socket closed due to Timeout");
+        }
+        servSock.close();
+        
+        System.out.println("Acknowledgement Protocol: " + ackProtocol);
+        System.out.println("Messages Read: " + msgCount);
+        System.out.println("Bytes Read: " + s.length());
+        
+
+    }
+    
+    public void run()
+    {
     	try {
     		int count = 0;
-    		double expectedSize = 0.0;
+    		int totalSize = 0;
+    		int ack = 0;
+    		int msgCount = 0;
     		byte[] clientInput = new byte[5];
+    		String ackProtocol = "Pure Streaming";
     		
-    		DataInputStream in = new DataInputStream(conn.getInputStream());
-
-            clientInput[0] = in.readByte();
-            clientInput[1] = in.readByte();
-            clientInput[2] = in.readByte();
-            clientInput[3] = in.readByte();
-            clientInput[4] = in.readByte();
+    		DataInputStream fromClient = new DataInputStream(conn.getInputStream());
+    		DataOutputStream toClient = null;
+    		
+            clientInput[0] = fromClient.readByte();
+            clientInput[1] = fromClient.readByte();
+            clientInput[2] = fromClient.readByte();
+            clientInput[3] = fromClient.readByte();
+            clientInput[4] = fromClient.readByte();
             ByteBuffer byteBuffer = ByteBuffer.wrap(clientInput, 1, 4);
 
-            int totalSize = byteBuffer.getInt();
-            System.out.println("About to read " + totalSize + " bytes");
+            totalSize = byteBuffer.getInt();
+            
+            if(clientInput[0] == 1) {
+                //Stop-and-wait protocol
+                toClient = new DataOutputStream(conn.getOutputStream());
+                ack = 1;
+                ackProtocol = "Stop-and-Wait";
+            }
 
-            byte[] messageByte = new byte[totalSize];
+            byte[] msg = new byte[totalSize];
             //The following code shows in detail how to read from a TCP socket
             String s = "";
             while(s.length() < totalSize)
             {
-                count = in.read(messageByte);
-                s += new String(messageByte, 0, count);
-
+                count = fromClient.read(msg);
+                s += new String(msg, 0, count);
+                msgCount++;
+                
+                if(ack == 1) {
+                    toClient.writeByte(1);
+                }
             }
            
     		conn.close();
+    		
+    		System.out.println("Acknowledgement Protocol: " + ackProtocol);
+    		System.out.println("Messages Read: " + msgCount);
+    		System.out.println("Bytes Read: " + s.length());
     	} catch (IOException e) {
     		System.err.println(e);
     	}
